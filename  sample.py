88314cb81baa00b2f91ef97037637ee2dd62162e
zoeybless <twainzsmith@yahoo.com>
2022-02-19 15:49:21 +0000

    first commit

---
+++  sample.py
@@ -0,0 +1,5 @@
+#jesus is Lord
+
+print (" Jesus is my everything love new me")
+print ("new line added")
+print ("four line added and will add fifth line")
---
+++ sample.pyx
@@ -0,0 +1,9 @@
+#jesus is Lord
+
+print (" Jesus is my everything love new me")
+print (" new line added")
+print ("four line added and will add fifth line")
+print ("new line added on devop branch ") 
+print ("changes made to this file")
+print (" now trying to creat another file ")
+print (" now trying to create another file ")
