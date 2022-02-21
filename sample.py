2573805265f3c4dcbc621c649e7e37bbcb9f779c
zoeybless <twainzsmith@yahoo.com>
2022-02-21 00:57:16 +0000

    first commit

--- sample.py
+++ sample.py
@@ -3,8 +3,5 @@
 print (" Jesus is my everything love new me")
 print (" new line added")
 print ("four line added and will add fifth line")
-<<<<<<< HEAD
-print ("new line added on devop branch ") 
-=======
+print ("new line added on devop branch ")
 print ("changes made to this file")
->>>>>>> ab9ae56a54cf2d60996766e66c0780eac890c8d8
